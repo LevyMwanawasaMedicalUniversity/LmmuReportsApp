16890996e578e6b4dbf77d748b4bf3fa409cb0d7
Azwel9nj <mulumbesimwinga@gmail.com>
2023-11-05 15:54:48 +0200

    email message updated

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -7,15 +7,20 @@
 <body>
     <div style="font-family: Arial, sans-serif; font-size: 16px; margin: 20px;">
         <b>
-            <h2>Exam Docket Notification</h2>
-            <p>Dear Student,</p>
-            <p>We are pleased to provide you with your exam docket, which is attached to this email. This docket has been awarded to you because your 2023 payments amount to 25% of the total payments required for your 2023 academic year. </p>
-            <p>Please review the document to ensure that all the details are accurate.</p>
-            <p>As part of our examination procedure, it is a requirement to have your docket stamped by visiting the Academic Office. This is an important step to ensure the validity of your exam docket.</p>
-            <p>If you find any errors or discrepancies, we kindly request that you visit the Academic Office to have them corrected. Your academic success is our priority, and we are here to assist you with any concerns you may have.</p>
-            <p>Please accept our apologies if you have already graduated or withdrawn from the university and should not be receiving this email. In such cases, please disregard this notification.</p>
-            <p>Best Regards,</p>
-            <p>LMMU ICT Team</p>
+            <b>
+                <h2>Exam Docket Notification</h2>
+                <p>Dear Student,</p>
+                <p>We are pleased to provide you with your exam docket, which is attached to this email. You have been awarded this docket because your 2023 payments account for 25% of the total payments required for the 2023 academic year.</p>
+                <p>Please review the document to ensure that all the details are accurate.</p>
+                <p>As part of our examination procedure, it is a requirement that you have a form of official identification, such as your student ID or National Registration Card, to use along with this docket. This is an important step to ensure the validity of your exam docket.</p>
+                <p>If you find any errors or discrepancies, we kindly request that you visit the Academic Office to have them corrected. Your academic success is our priority, and we are here to assist you with any concerns you may have.</p>
+                <p>You can also access your docket by visiting our docket printing dashboard at <a href="http://sisreports.lmmu.ac.zm/">http://sisreports.lmmu.ac.zm/</a>.</p>
+                <p>You can log in to the system using your student number as the username (e.g., 123456789@lmmu.ac.zm) and your National Registration Card (NRC) number as the password.</p>
+                <p>Do not forget to add the "@lmmu.ac.zm" at the end of your student number when logging in</p>
+                <p>Please accept our apologies if you have already graduated or withdrawn from the university and should not be receiving this email. In such cases, please disregard this notification.</p>
+                <p>Best Regards,</p>
+                <p>LMMU ICT Team</p>
+            </b>
         </b>
     </div>
 </body>
