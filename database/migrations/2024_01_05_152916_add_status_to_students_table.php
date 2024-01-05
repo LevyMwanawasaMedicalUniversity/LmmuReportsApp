d784fa0b26e3cb11009e01f20441dbc730c04357
Azwel9nj <mulumbesimwinga@gmail.com>
2024-01-05 17:30:09 +0200

    migration added

--- database/migrations/2024_01_05_152916_add_status_to_students_table.php
+++ database/migrations/2024_01_05_152916_add_status_to_students_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('students', function (Blueprint $table) {
-            $table->integer('status')->default(1)->nullable(false);
+            $table->integer('status')->default(1);
         });
     }
 
