c74a51ff7549f9627a55083a9a0025d5099e9fb1
Azwel9nj <mulumbesimwinga@gmail.com>
2024-02-08 08:28:03 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 ])
 
 @section('content')
-  <div class="panel-header panel-header-lg">
+  <!-- <div class="panel-header panel-header-lg">
     <canvas id="bigDashboardChart"></canvas>
   </div>
   <div class="content">
@@ -270,7 +270,26 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
+  <div class="panel-header panel-header-sm">
+    
+    </div>
+   
+    <div class="content">
+        <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header">
+                  <centre><h4 class="card-title">Welcome To The Reports Dashboard</h4></centre>
+                </div>
+                <div class="card-body">
+                  <img src="{{ asset('assets') }}/img/DJI_0097.JPG" alt="LMMU">
+                  {{-- <img src="{{ asset('images/DJI_0097.jpg') }}" alt="Description of your image"> --}}
+                </div>
+              </div>
+            </div>
+        </div>
+      </div> 
 @endsection
 
 @push('js')
