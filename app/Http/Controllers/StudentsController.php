b898c28b36925fe6effcb0d2fcf66b9b9d1d6ad4
Azwel9nj <mulumbesimwinga@gmail.com>
2024-03-07 10:42:38 +0200

    fdfd

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -100,20 +100,27 @@ class StudentsController extends Controller
     
     private function validateAndPrepareEmail($email, $studentId) {
         $email = trim($email);
+    
         // Check if the email is empty
         if (empty($email)) {
             $email = $studentId . '@lmmu.ac.zm';
         } else {
             $email = trim($email);
+    
+            // Check if the email already exists for another user
             $existingUser = User::where('email', $email)->where('name', '!=', $studentId)->first();
+    
             if ($existingUser) {
                 // If the email exists for another user, append the student ID
                 $email = $studentId . $email . '@lmmu.ac.zm';
             } elseif (!User::where('name', $studentId)->where('email', $email)->exists()) {
                 // If the email doesn't exist for the current user, update it
                 User::where('name', $studentId)->update(['email' => $email]);
+            } else {
+                // If the email exists for the current user, do nothing
             }
         }
+    
         return $email;
     }
 
