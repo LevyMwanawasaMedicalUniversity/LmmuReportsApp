4531fdc471a43038b283273f7edd5a1c86009f95
Azwel9nj <mulumbesimwinga@gmail.com>
2024-04-17 11:28:14 +0200

    UPDATE

--- resources/views/docket/show.blade.php
+++ resources/views/docket/show.blade.php
@@ -75,7 +75,7 @@
                                 <div style="font-size: 15pt; font-weight: bold;">EXAMINATION DOCKET 2024 </div>
                                 @else
                                 <div style="font-size: 15pt; font-weight: bold;">FINAL EXAMINATION DOCKET 2024 </div>
-                                <div style="font-size: 15pt; font-weight: bold;">UNREGISTERED STUDENT </div>
+                                {{-- <div style="font-size: 15pt; font-weight: bold;">UNREGISTERED STUDENT </div> --}}
                                 @endif
                                 <div style="font-size: 10pt; font-weight: bold;">PRINTED OUT</h2>
                                 <BR>
