73814c9039e923df648b0a810973baf93db75661
Azwel9nj <mulumbesimwinga@gmail.com>
2024-09-19 16:07:53 +0200

    corrected

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,8 @@
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="{{ asset('assets') }}/demo/demo.css" rel="stylesheet" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
   
 </head>
 
