5d1ea618241459ddf861c62e2ba481ff7d083764
Azwel9nj <mulumbesimwinga@gmail.com>
2024-10-11 14:05:31 +0200

    No more pulsating

--- resources/views/allStudents/continousAssessment/viewCa.blade.php
+++ resources/views/allStudents/continousAssessment/viewCa.blade.php
@@ -6,7 +6,7 @@
 ])
 
 @section('content')
-<style>
+{{-- <style>
 @keyframes pulsate {
     0% {
         transform: scale(1);
@@ -22,7 +22,7 @@
 .pulsate {
     animation: pulsate 1s infinite;
 }
-</style>
+</style> --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div class="panel-header panel-header-sm">
