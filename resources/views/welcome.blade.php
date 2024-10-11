4143c70ca4eb231df2ad149ee437e40b010ce1a9
Azwel9nj <mulumbesimwinga@gmail.com>
2024-10-11 12:25:43 +0200

    SIS REPORTS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
                   
                       <div class="row justify-content-center">
                           <div class="col-lg-12 col-md-9">
-                              <h3 class="text-white">{{ __('Welcome to the LMMU report app dashboard') }}</h3>
+                              <h3 class="text-white">{{ __('Welcome to SIS REPORTS') }}</h3>
                               <p class="text-lead text-light mt-3 mb-0">
                                   @include('alerts.migrations_check')
                               </p>
