c204ebf8022d1edf795be1f23b3651c978d6106e
Azwel9nj <mulumbesimwinga@gmail.com>
2024-12-27 15:22:56 +0200

    Update sidebar link for student exam results to use URL helper

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -35,7 +35,7 @@
           </a>
         </li>
         <li class = "@if ($activePage == 'studentExaminationResults') active @endif">
-          <a href="{{ route('studentsExamResults' , auth()->user()->name) }}">
+          <a href="{{ url('/students/exam/results/' . auth()->user()->name) }}">
             <i class="now-ui-icons education_hat"></i>
             <p>{{ __('Exam Results') }}</p>
           </a>
